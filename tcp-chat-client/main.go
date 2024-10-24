e75ae5cf436dd5ef074342cc31d11864ec6aa997
zaindeveloper2024 <zain.developer2024@gmail.com>
2024-10-24 19:41:11 +0700

    tcp chat app

---
+++ tcp-chat-client/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+type Message struct {
+	Type    string      `json:"type"`
+	Content interface{} `json:"content"`
+}
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Printf("Error dialing: %s", err.Error())
+		return
+	}
+
+	defer conn.Close()
+
+	go receiveMessages(conn)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Println("Enter message (or 'quit' to exit):")
+		if !scanner.Scan() {
+			log.Printf("Error reading from stdin: %s", scanner.Err())
+			break
+		}
+
+		text := scanner.Text()
+		if strings.ToLower(text) == "quit" {
+			log.Println("Exiting...")
+			return
+		}
+
+		msg := Message{
+			Type:    "chat",
+			Content: text,
+		}
+
+		encoder := json.NewEncoder(conn)
+		if err := encoder.Encode(msg); err != nil {
+			fmt.Println("Error encoding message:", err)
+			return
+		}
+	}
+}
+
+func receiveMessages(conn net.Conn) {
+	decoder := json.NewDecoder(conn)
+	for {
+		var msg Message
+		if err := decoder.Decode(&msg); err != nil {
+			fmt.Println("Connection closed by server")
+			os.Exit(0)
+		}
+		fmt.Printf("\nReceived: %+v\n", msg)
+		fmt.Print("Enter message (or 'quit' to exit): ")
+	}
+}
---
+++ tcp-chat-server/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+)
+
+type Message struct {
+	Type    string      `json:"type"`
+	Content interface{} `json:"content"`
+}
+
+type Server struct {
+	clients    map[net.Conn]bool
+	clientsMux sync.RWMutex
+}
+
+func NewServer() *Server {
+	return &Server{
+		clients: make(map[net.Conn]bool),
+	}
+}
+
+func main() {
+	server := NewServer()
+
+	serverAddr := "localhost:8080"
+	listener, err := net.Listen("tcp", serverAddr)
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Printf("Server is listening on %s\n", serverAddr)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting:", err)
+			continue
+		}
+		go server.handleClient(conn)
+	}
+}
+
+func (s *Server) handleClient(conn net.Conn) {
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Printf("Error closing connection: %v\n", err)
+		}
+	}()
+
+	// Client
+	s.clientsMux.Lock()
+	s.clients[conn] = true
+	s.clientsMux.Unlock()
+
+	clientAddr := conn.RemoteAddr().String()
+	log.Printf("Accepted connection from %s\n", clientAddr)
+
+	defer func() {
+		s.clientsMux.Lock()
+		delete(s.clients, conn)
+		s.clientsMux.Unlock()
+	}()
+
+	decoder := json.NewDecoder(conn)
+	for {
+		var msg Message
+		if err := decoder.Decode(&msg); err != nil {
+			fmt.Printf("Client disconnected: %v\n", err)
+			return
+		}
+
+		fmt.Printf("Received message: %v\n", msg)
+
+		s.broadcast(msg, conn)
+	}
+}
+
+func (s *Server) broadcast(msg Message, sender net.Conn) {
+	s.clientsMux.RLock()
+	defer s.clientsMux.RUnlock()
+
+	for client := range s.clients {
+		if client != sender {
+			encoder := json.NewEncoder(client)
+			if err := encoder.Encode(msg); err != nil {
+				fmt.Printf("Error broadcasting to client: %v\n", err)
+			}
+		}
+	}
+}
